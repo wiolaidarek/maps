ed85b762c59c1c854934cee46f9426b8e7fcd274
dizak <dariusz.izak@ibb.waw.pl>
2019-06-20 23:57:40 +0200

    Initial commit

---
+++ maps.js
@@ -0,0 +1,37 @@
+var tatrzanski_bor = {
+	'position': {lat: 49.311799, lng: 20.061783},
+	'label': 'Tatrzański Bór - przyjęcie',
+}
+var wiktorowki = {
+	'position': {lat: 49.265404, lng: 20.086490},
+	'label': 'Sanktuarium Wiktorówki - ślub',
+}
+
+function myMap() {
+  var mapProp = {
+    disableDefaultUI: true,
+    center: new google.maps.LatLng(49.31,20.05),
+    zoom: 12,
+    zoomControl: true,
+    scaleControl: true,
+    rotateControl: true,
+    mapTypeId: 'terrain',
+  };
+
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+  var marker = new google.maps.Marker(
+	  {
+		position: tatrzanski_bor['position'],
+		label: tatrzanski_bor['label'],
+		map: map,
+	  }
+  ) 
+  var marker = new google.maps.Marker(
+	  {
+		position: wiktorowki['position'],
+		label: wiktorowki['label'], 
+		map: map,
+	  }
+  ) 
+}
